ca225bd644d3c290a4eb2c8d854bcaae11cd3bfd
Bozhidara12 <bozidaralazarova@gmail.com>
2024-04-30 12:16:29 +0300

    to insert

---
+++ Connection Tables/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Connection_Tables
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt1 = new TextBox();
+            lbl1 = new Label();
+            txt2 = new TextBox();
+            lbl2 = new Label();
+            btn1 = new Button();
+            SuspendLayout();
+            // 
+            // txt1
+            // 
+            txt1.Location = new Point(97, 72);
+            txt1.Name = "txt1";
+            txt1.Size = new Size(118, 23);
+            txt1.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(97, 54);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(39, 15);
+            lbl1.TabIndex = 1;
+            lbl1.Text = "Name";
+            // 
+            // txt2
+            // 
+            txt2.Location = new Point(421, 71);
+            txt2.Name = "txt2";
+            txt2.Size = new Size(135, 23);
+            txt2.TabIndex = 2;
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Location = new Point(421, 53);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(59, 15);
+            lbl2.TabIndex = 3;
+            lbl2.Text = "Name_BG";
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(239, 215);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(241, 51);
+            btn1.TabIndex = 4;
+            btn1.Text = "Insert into rod";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn1);
+            Controls.Add(lbl2);
+            Controls.Add(txt2);
+            Controls.Add(lbl1);
+            Controls.Add(txt1);
+            Name = "Form1";
+            Text = "Connecting Tables";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt1;
+        private Label lbl1;
+        private TextBox txt2;
+        private Label lbl2;
+        private Button btn1;
+    }
+}
---
+++ Trees_Zaimov/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Trees_Zaimov
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl1 = new Label();
+            txt1 = new TextBox();
+            lbl2 = new Label();
+            cmb1 = new ComboBox();
+            lbl3 = new Label();
+            cmb2 = new ComboBox();
+            cmb3 = new ComboBox();
+            cmb4 = new ComboBox();
+            cmb5 = new ComboBox();
+            cmb6 = new ComboBox();
+            lbl4 = new Label();
+            lbl5 = new Label();
+            lbl6 = new Label();
+            lbl7 = new Label();
+            lbl8 = new Label();
+            txt2 = new TextBox();
+            txt3 = new TextBox();
+            pictureBox1 = new PictureBox();
+            btn1 = new Button();
+            btn2 = new Button();
+            lbl9 = new Label();
+            lbl10 = new Label();
+            txt5 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Location = new Point(34, 13);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(90, 15);
+            lbl1.TabIndex = 0;
+            lbl1.Text = "Наименование";
+            // 
+            // txt1
+            // 
+            txt1.Location = new Point(34, 49);
+            txt1.Name = "txt1";
+            txt1.Size = new Size(189, 23);
+            txt1.TabIndex = 1;
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Location = new Point(34, 84);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(36, 15);
+            lbl2.TabIndex = 2;
+            lbl2.Text = "Otdel";
+            // 
+            // cmb1
+            // 
+            cmb1.FormattingEnabled = true;
+            cmb1.Location = new Point(34, 111);
+            cmb1.Name = "cmb1";
+            cmb1.Size = new Size(121, 23);
+            cmb1.TabIndex = 3;
+            // 
+            // lbl3
+            // 
+            lbl3.AutoSize = true;
+            lbl3.Location = new Point(36, 148);
+            lbl3.Name = "lbl3";
+            lbl3.Size = new Size(34, 15);
+            lbl3.TabIndex = 4;
+            lbl3.Text = "Class";
+            // 
+            // cmb2
+            // 
+            cmb2.FormattingEnabled = true;
+            cmb2.Location = new Point(34, 166);
+            cmb2.Name = "cmb2";
+            cmb2.Size = new Size(121, 23);
+            cmb2.TabIndex = 5;
+            // 
+            // cmb3
+            // 
+            cmb3.FormattingEnabled = true;
+            cmb3.Location = new Point(34, 215);
+            cmb3.Name = "cmb3";
+            cmb3.Size = new Size(121, 23);
+            cmb3.TabIndex = 6;
+            // 
+            // cmb4
+            // 
+            cmb4.FormattingEnabled = true;
+            cmb4.Location = new Point(34, 288);
+            cmb4.Name = "cmb4";
+            cmb4.Size = new Size(121, 23);
+            cmb4.TabIndex = 7;
+            // 
+            // cmb5
+            // 
+            cmb5.FormattingEnabled = true;
+            cmb5.Location = new Point(34, 341);
+            cmb5.Name = "cmb5";
+            cmb5.Size = new Size(121, 23);
+            cmb5.TabIndex = 8;
+            // 
+            // cmb6
+            // 
+            cmb6.FormattingEnabled = true;
+            cmb6.Location = new Point(34, 406);
+            cmb6.Name = "cmb6";
+            cmb6.Size = new Size(121, 23);
+            cmb6.TabIndex = 9;
+            // 
+            // lbl4
+            // 
+            lbl4.AutoSize = true;
+            lbl4.Location = new Point(36, 197);
+            lbl4.Name = "lbl4";
+            lbl4.Size = new Size(42, 15);
+            lbl4.TabIndex = 10;
+            lbl4.Text = "Razred";
+            // 
+            // lbl5
+            // 
+            lbl5.AutoSize = true;
+            lbl5.Location = new Point(38, 259);
+            lbl5.Name = "lbl5";
+            lbl5.Size = new Size(42, 15);
+            lbl5.TabIndex = 11;
+            lbl5.Text = "Family";
+            // 
+            // lbl6
+            // 
+            lbl6.AutoSize = true;
+            lbl6.Location = new Point(36, 314);
+            lbl6.Name = "lbl6";
+            lbl6.Size = new Size(28, 15);
+            lbl6.TabIndex = 12;
+            lbl6.Text = "Rod";
+            // 
+            // lbl7
+            // 
+            lbl7.AutoSize = true;
+            lbl7.Location = new Point(38, 388);
+            lbl7.Name = "lbl7";
+            lbl7.Size = new Size(31, 15);
+            lbl7.TabIndex = 13;
+            lbl7.Text = "Type";
+            // 
+            // lbl8
+            // 
+            lbl8.AutoSize = true;
+            lbl8.Location = new Point(290, 13);
+            lbl8.Name = "lbl8";
+            lbl8.Size = new Size(49, 15);
+            lbl8.TabIndex = 14;
+            lbl8.Text = "SelectID";
+            lbl8.Visible = false;
+            // 
+            // txt2
+            // 
+            txt2.Location = new Point(262, 49);
+            txt2.Name = "txt2";
+            txt2.Size = new Size(107, 23);
+            txt2.TabIndex = 15;
+            txt2.Visible = false;
+            // 
+            // txt3
+            // 
+            txt3.Location = new Point(408, 49);
+            txt3.Multiline = true;
+            txt3.Name = "txt3";
+            txt3.ScrollBars = ScrollBars.Vertical;
+            txt3.ShortcutsEnabled = false;
+            txt3.Size = new Size(204, 334);
+            txt3.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(618, 166);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(146, 135);
+            pictureBox1.TabIndex = 17;
+            pictureBox1.TabStop = false;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(217, 111);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(152, 65);
+            btn1.TabIndex = 18;
+            btn1.Text = "Add new tree";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(217, 215);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(152, 63);
+            btn2.TabIndex = 19;
+            btn2.Text = "UPDATE selected tree";
+            btn2.UseVisualStyleBackColor = true;
+            // 
+            // lbl9
+            // 
+            lbl9.AutoSize = true;
+            lbl9.Location = new Point(434, 9);
+            lbl9.Name = "lbl9";
+            lbl9.Size = new Size(144, 15);
+            lbl9.TabIndex = 20;
+            lbl9.Text = "Описание/информация/";
+            // 
+            // lbl10
+            // 
+            lbl10.AutoSize = true;
+            lbl10.Location = new Point(229, 314);
+            lbl10.Name = "lbl10";
+            lbl10.Size = new Size(108, 15);
+            lbl10.TabIndex = 22;
+            lbl10.Text = "Добави image URL";
+            // 
+            // txt5
+            // 
+            txt5.Location = new Point(229, 341);
+            txt5.Name = "txt5";
+            txt5.Size = new Size(100, 23);
+            txt5.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txt5);
+            Controls.Add(lbl10);
+            Controls.Add(lbl9);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(pictureBox1);
+            Controls.Add(txt3);
+            Controls.Add(txt2);
+            Controls.Add(lbl8);
+            Controls.Add(lbl7);
+            Controls.Add(lbl6);
+            Controls.Add(lbl5);
+            Controls.Add(lbl4);
+            Controls.Add(cmb6);
+            Controls.Add(cmb5);
+            Controls.Add(cmb4);
+            Controls.Add(cmb3);
+            Controls.Add(cmb2);
+            Controls.Add(lbl3);
+            Controls.Add(cmb1);
+            Controls.Add(lbl2);
+            Controls.Add(txt1);
+            Controls.Add(lbl1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl1;
+        private TextBox txt1;
+        private Label lbl2;
+        private ComboBox cmb1;
+        private Label lbl3;
+        private ComboBox cmb2;
+        private ComboBox cmb3;
+        private ComboBox cmb4;
+        private ComboBox cmb5;
+        private ComboBox cmb6;
+        private Label lbl4;
+        private Label lbl5;
+        private Label lbl6;
+        private Label lbl7;
+        private Label lbl8;
+        private TextBox txt2;
+        private TextBox txt3;
+        private PictureBox pictureBox1;
+        private Button btn1;
+        private Button btn2;
+        private Label lbl9;
+        private Label lbl10;
+        private TextBox txt5;
+    }
+}
